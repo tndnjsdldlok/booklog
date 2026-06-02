dd354ce270a5872adce87e7d63f16a1b1eae90a0
tndnjsdldlok <tndnjsdldl@gmail.com>
2026-06-02 22:25:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'booklog-v1';
+const CACHE = 'booklog-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@300;400;600&family=Gowun+Batang:wght@400;700&family=DM+Mono:ital,wght@0,300;1,300&display=swap'
+  '/booklog/',
+  '/booklog/index.html',
+  '/booklog/manifest.json'
 ];
 
 self.addEventListener('install', e => {
@@ -21,6 +21,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/booklog/index.html')))
   );
 });
